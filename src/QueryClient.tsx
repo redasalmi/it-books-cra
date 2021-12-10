0fe1e0ef1af4026f222e95c7c533c83f5b5f2eb1
redasalmi <reda.salmi.elt@gmail.com>
2021-12-10 19:02:49 +0100

    added typescript and switched main files

--- src/QueryClient.tsx
+++ src/QueryClient.tsx
@@ -1,9 +1,13 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
+interface AppQueryClientProviderProps {
+  children: React.ReactNode;
+}
+
 const queryClient = new QueryClient();
 
-const AppQueryClientProvider = ({ children }) => (
+const AppQueryClientProvider = ({ children }: AppQueryClientProviderProps) => (
   <QueryClientProvider client={queryClient}>
     {children}
     <ReactQueryDevtools initialIsOpen={false} />
