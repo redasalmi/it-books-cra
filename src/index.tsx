514c6f7f5f03c5e84d1a4bb94792e94bb0218a25
redasalmi <reda.salmi.elt@gmail.com>
2021-12-21 17:18:52 +0100

    replaced local fonts with @fontsource/poppins

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import App from '~/app';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/700.css';
 import '~/styles/global.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
